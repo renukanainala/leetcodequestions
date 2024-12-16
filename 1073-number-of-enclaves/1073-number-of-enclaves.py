5fd2e697893d9829ce717571a093450d3d9599b7
renukanainala <142616816+renukanainala@users.noreply.github.com>
2024-12-16 20:20:16 +0530

    Time: 96 ms (50.51%), Space: 24.7 MB (76.45%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -6,14 +6,14 @@ class Solution:
         s=set()
         vr=[0,-1,0,1]
         vc=[-1,0,1,0]
-        l=[0]
+        self.l=0
         def bfs(i,j):
             q=deque()
             q.append((i,j))
             s.add((i,j))
             while q:
                 r,c=q.popleft()
-                l[0]-=1
+                self.l-=1
                 for k in range(4):
                     r1,c1=r+vr[k],c+vc[k]
                     if 0<=r1<row and 0<=c1<col and g[r1][c1]==1 and (r1,c1) not in s:
@@ -24,7 +24,7 @@ class Solution:
         for i in range(row):
             for j in range(col):
                 if g[i][j]==1:
-                    l[0]+=1
+                    self.l+=1
         for i in range(col):
             if g[0][i]==1 and (0,i) not in s:
                 bfs(0,i)
@@ -35,5 +35,5 @@ class Solution:
                 bfs(i,0)
             if g[i][col-1]==1 and (i,col-1) not in s:
                 bfs(i,col-1)
-        return l[0]
+        return self.l
         
