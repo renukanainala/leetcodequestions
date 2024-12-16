1e7f6461fc220aca46d93a8928095285fa03c268
renukanainala <142616816+renukanainala@users.noreply.github.com>
2024-12-16 20:19:22 +0530

    Time: 100 ms (47.69%), Space: 24.9 MB (74.52%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numEnclaves(self, g: List[List[int]]) -> int:
+        c=0
+        row=len(g)
+        col=len(g[0])
+        s=set()
+        vr=[0,-1,0,1]
+        vc=[-1,0,1,0]
+        l=[0]
+        def bfs(i,j):
+            q=deque()
+            q.append((i,j))
+            s.add((i,j))
+            while q:
+                r,c=q.popleft()
+                l[0]-=1
+                for k in range(4):
+                    r1,c1=r+vr[k],c+vc[k]
+                    if 0<=r1<row and 0<=c1<col and g[r1][c1]==1 and (r1,c1) not in s:
+                        q.append((r1,c1))
+                        
+                        s.add((r1,c1))
+                
+        for i in range(row):
+            for j in range(col):
+                if g[i][j]==1:
+                    l[0]+=1
+        for i in range(col):
+            if g[0][i]==1 and (0,i) not in s:
+                bfs(0,i)
+            if g[row-1][i]==1 and (row-1,i) not in s:
+                bfs(row-1,i)
+        for i in range(row):
+            if g[i][0]==1 and (i,0) not in s:
+                bfs(i,0)
+            if g[i][col-1]==1 and (i,col-1) not in s:
+                bfs(i,col-1)
+        return l[0]
+        
