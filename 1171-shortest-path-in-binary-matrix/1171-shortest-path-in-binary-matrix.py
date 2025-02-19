4399705d231744d7d8a63f982122cc394ee7b018
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-02-19 23:09:35 +0530

    Time: 264 ms (28.88%), Space: 19.6 MB (29.54%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,21 +1,26 @@
 class Solution:
+    import heapq
     def shortestPathBinaryMatrix(self, g: List[List[int]]) -> int:
+        n=len(g)
+        m=len(g[0])
+        h=[]
         if g[0][0]==1:
             return -1
-        q=deque([(0,0,1)])
+        heapq.heappush(h,[1,0,0])
         s=set()
         s.add((0,0))
-        vr=[0,-1,-1,-1,0,1,1,1]
-        vc=[-1,-1,0,1,1,1,0,-1]
-        row=len(g)
-        col=len(g[0])
-        while q:
-            r,c,l=q.popleft()
-            if r==row-1 and c==col-1:
-                return l
+        nr=[0,-1,-1,-1,0,1,1,1]
+        nc=[-1,-1,0,1,1,1,0,-1]
+        while h:
+            d,r,c=heapq.heappop(h)
+            if r==n-1 and c==n-1:
+                return d
             for i in range(8):
-                r1,c1=r+vr[i],c+vc[i]
-                if 0<=r1<row and 0<=c1<col and g[r1][c1]==0 and (r1,c1) not in s:
-                    s.add((r1,c1))
-                    q.append((r1,c1,l+1))
+                nrow=r+nr[i]
+                ncol=c+nc[i]
+                if 0<=nrow<n and 0<=ncol<m  and g[nrow][ncol]==0 and (nrow,ncol) not in s:
+                    heapq.heappush(h,[d+1,nrow,ncol])
+                    s.add((nrow,ncol))
         return -1
+
+
