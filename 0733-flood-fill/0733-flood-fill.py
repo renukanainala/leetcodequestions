5a229349a936e23ad6303cf57c71f88fb15a49f1
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-02-28 18:16:10 +0530

    Time: 0 ms (100%), Space: 18.2 MB (26.39%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,28 @@
+class Solution:
+    def floodFill(self, g: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        n=len(g)
+        m=len(g[0])
+        s=set()
+        nr=[0,-1,0,1]
+        nc=[-1,0,1,0]
+        def bfs(i,j):
+            q=collections.deque()
+            q.append((i,j))
+            s.add((i,j))
+            e=g[i][j]
+            g[i][j]=color
+            val=g[i][j]
+            while q:
+                r,c=q.popleft()
+                for i in range(4):
+                    r1,c1=nr[i]+r,nc[i]+c
+                    if 0<=r1<n and 0<=c1<m and (r1,c1) not in s and g[r1][c1]==e:
+                        g[r1][c1]=val
+                        q.append((r1,c1))
+                        s.add((r1,c1))
+
+        if g[sr][sc]==color:
+            return g
+        bfs(sr,sc)
+        return g
+        
