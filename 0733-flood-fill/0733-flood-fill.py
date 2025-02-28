81fdfb7a84ab8dab282b998a8b79bf893f58c333
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-02-28 18:46:55 +0530

    Time: 2 ms (20.58%), Space: 18.1 MB (26.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,8 +5,8 @@ class Solution:
         s=set()
         nr=[0,-1,0,1]
         nc=[-1,0,1,0]
-        def bfs(i,j):
-            q=collections.deque()
+        def bfs(i,j,e,color):
+            '''q=collections.deque()
             q.append((i,j))
             s.add((i,j))
             e=g[i][j]
@@ -19,10 +19,19 @@ class Solution:
                     if 0<=r1<n and 0<=c1<m and (r1,c1) not in s and g[r1][c1]==e:
                         g[r1][c1]=val
                         q.append((r1,c1))
-                        s.add((r1,c1))
+                        s.add((r1,c1))'''
+            if i<0 or i>=n or j<0 or j>=m or (i,j) in s or g[i][j]!=e:
+                return
+            s.add((i,j))
+            g[i][j]=color
+            bfs(i,j-1,e,color)
+            bfs(i,j+1,e,color)
+            bfs(i-1,j,e,color)
+            bfs(i+1,j,e,color)
 
         if g[sr][sc]==color:
             return g
-        bfs(sr,sc)
+        e=g[sr][sc]
+        bfs(sr,sc,e,color)
         return g
         
