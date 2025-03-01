33eaa2bc52c9f38e90f127912af9da235b451ebe
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-03-01 07:14:00 +0530

    Time: 75 ms (69.32%), Space: 19.9 MB (87.27%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -3,27 +3,28 @@ class Solution:
         c=0
         row=len(g)
         col=len(g[0])
-        s=set()
+        #s=set()
         vr=[0,-1,0,1]
         vc=[-1,0,1,0]
         c=0
         def bfs(i,j):
             q=deque()
             q.append((i,j))
-            s.add((i,j))
+            g[i][j]=0
+           # s.add((i,j))
             while q:
                 r,c=q.popleft()
                # self.l-=1
                 for k in range(4):
                     r1,c1=r+vr[k],c+vc[k]
-                    if 0<=r1<row and 0<=c1<col and g[r1][c1]==1 and (r1,c1) not in s:
+                    if 0<=r1<row and 0<=c1<col and g[r1][c1]==1 :#and (r1,c1) not in s:
                         q.append((r1,c1))  
-                        s.add((r1,c1))
+                        g[r1][c1]=0
                 
         for i in range(row):
             for j in range(col):
                 if i==0 or i==row-1 or j==0 or j==col-1:
-                    if g[i][j]==1 and (i,j) not in s:
+                    if g[i][j]==1 :#and (i,j) not in s:
                         bfs(i,j)
         '''for i in range(col):
             if g[0][i]==1 and (0,i) not in s:
@@ -38,7 +39,7 @@ class Solution:
         return self.l-len(s)'''
         for i in range(row):
             for j in range(col):
-                if g[i][j]==1 and (i,j) not in s:
+                if g[i][j]==1 :#and (i,j) not in s:
                     c+=1
         return c
         
