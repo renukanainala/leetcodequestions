033ad279a8d8caf9a967ad0b31440c492a310611
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-04-23 23:23:13 +0530

    Time: 86 ms (5%), Space: 18 MB (40.5%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.py
+++ 1500-count-largest-group/1500-count-largest-group.py
@@ -1,15 +1,15 @@
 class Solution:
     from collections import defaultdict
     def countLargestGroup(self, n: int) -> int:
-        h=defaultdict(list)
+        h=defaultdict(int)
         l=0
         for i in range(1,n+1):
             s=[int(j) for j in str(i)]
-            h[sum(s)].append(i)
-            l=max(l,len(h[sum(s)]))
+            h[sum(s)]=h[sum(s)]+1
+            l=max(l,h[sum(s)])
         c=0
         for j in h.values():
-            if len(j)==l:
+            if j==l:
                 c+=1
         return c
              
