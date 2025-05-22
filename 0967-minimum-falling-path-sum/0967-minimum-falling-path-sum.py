947111cc1be13eaa665d0fbc9e3382405cc842ae
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-05-22 21:43:26 +0530

    Time: 24 ms (49.29%), Space: 18.7 MB (80.32%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -1,21 +1,14 @@
 class Solution:
     def minFallingPathSum(self, a: List[List[int]]) -> int:
-        def path(i,j,dp):
-            if j<0 or j>=n or i>=n:
-                return float('inf')
-            if i==0:
-                return a[0][j]
-            if dp[i][j]!='s':
-                return dp[i][j]
-            up=path(i-1,j,dp)
-            ld=path(i-1,j-1,dp)
-            rd=path(i-1,j+1,dp)
-            dp[i][j]=a[i][j]+min(up,ld,rd)
-            return dp[i][j]
-        mini=float('inf')
         n=len(a)
-        dp=[['s']*n for i in range(n)]
+        d=[['s']*n for i in range(n)]
         for i in range(n):
-            mini=min(mini,path(n-1,i,dp))
-        return mini
+            d[0][i]=a[0][i]
+        for i in range(1,n):
+            for j in range(n):
+                up=d[i-1][j] if i>=0 else float('inf')
+                ld=d[i-1][j-1] if (i>=0 and j>0) else float('inf')
+                rd=d[i-1][j+1] if (i>0 and j>=0 and j<n-1) else float('inf')
+                d[i][j]=a[i][j]+min(up,min(ld,rd))
+        return min(d[n-1])
 
