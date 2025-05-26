4acf55d69fd39e78202ef64f0f814bcf2ebe192b
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-05-26 14:39:38 +0530

    Time: 1350 ms (9.28%), Space: 21.4 MB (23.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,6 +1,6 @@
 class Solution:
-    def coinChange(self, ar: List[int], amount: int) -> int:
-        def find(ind,t,dp):
+    def coinChange(self, ar: List[int], t: int) -> int:
+        '''def find(ind,t,dp):
             if t==0:
                 return 0
             if ind==0:
@@ -15,9 +15,21 @@ class Solution:
             if ar[ind]<=t:
                 ta=1+find(ind,t-ar[ind],dp)
             dp[ind][t]=min(ta,nt)
-            return dp[ind][t]
-        dp=[[-1]*(amount+1) for j in range(len(ar))]
-        res=find(len(ar)-1,amount,dp) 
-        if res==float('inf') :
-            return -1 
-        return res   
+            return dp[ind][t]'''
+        n=len(ar)
+        dp=[[-1]*(t+1) for j in range(len(ar))]
+        for i in range(t+1):
+            if i%ar[0]==0:
+                dp[0][i]=i//ar[0]
+            else:
+                dp[0][i]=float('inf')
+        for i in range(n):
+            dp[i][0]=0
+        for i in range(1,len(ar)):
+            for j in range(1,t+1):
+                nt=0+dp[i-1][j]
+                ta=float('inf')
+                if ar[i]<=j:
+                    ta=1+dp[i][j-ar[i]] 
+                dp[i][j]=min(ta,nt)
+        return -1 if dp[n-1][t]==float('inf') else dp[n-1][t]
