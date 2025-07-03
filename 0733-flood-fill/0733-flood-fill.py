73a15c52ade8c733bcb2252a0ebd527d97971acc
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-07-03 11:24:21 +0530

    Time: 0 ms (100%), Space: 18.2 MB (28.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,33 +5,23 @@ class Solution:
         s=set()
         nr=[0,-1,0,1]
         nc=[-1,0,1,0]
-        def bfs(i,j,e,color):
-            '''q=collections.deque()
+        def bfs(i,j):
+            q=collections.deque()
             q.append((i,j))
             s.add((i,j))
             e=g[i][j]
             g[i][j]=color
-            val=g[i][j]
             while q:
                 r,c=q.popleft()
                 for i in range(4):
                     r1,c1=nr[i]+r,nc[i]+c
                     if 0<=r1<n and 0<=c1<m and (r1,c1) not in s and g[r1][c1]==e:
-                        g[r1][c1]=val
+                        g[r1][c1]=color
                         q.append((r1,c1))
-                        s.add((r1,c1))'''
-            if i<0 or i>=n or j<0 or j>=m or (i,j) in s or g[i][j]!=e:
-                return
-            s.add((i,j))
-            g[i][j]=color
-            bfs(i,j-1,e,color)
-            bfs(i,j+1,e,color)
-            bfs(i-1,j,e,color)
-            bfs(i+1,j,e,color)
+                        s.add((r1,c1))
 
         if g[sr][sc]==color:
             return g
-        e=g[sr][sc]
-        bfs(sr,sc,e,color)
+        bfs(sr,sc)
         return g
         
