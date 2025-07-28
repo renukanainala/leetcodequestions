2ed06efab05c74935727b53c5fa40ee5b8105b8f
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-07-28 15:21:06 +0530

    Time: 190 ms (91.45%), Space: 33.7 MB (6.52%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,15 +1,18 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int: 
-        def find(i,s,nums,j):
+        def find(i,s,nums,j,dp):
             if i<0:
-                return s==j    
-            l=find(i-1,nums[i]|s,nums,j)
-            h=find(i-1,s,nums,j)
-            return l+h       
+                return s==j  
+            if dp[i][s]!=-1 :
+                return dp[i][s]
+            l=find(i-1,nums[i]|s,nums,j,dp)
+            h=find(i-1,s,nums,j,dp)
+            dp[i][s]= l+h    
+            return  dp[i][s]  
         maxi=0
         for i in nums: 
             maxi=maxi|i
         s=0
-        
-        return find(len(nums)-1,s,nums,maxi)
+        dp=[[-1] * (maxi+1) for i in range(len(nums)) ]
+        return find(len(nums)-1,s,nums,maxi,dp)
         
