21d6d1e41d7ba7a51609f8b7401fbe55ec2ed164
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-08-16 22:30:58 +0530

    Time: 0 ms (100%), Space: 17.7 MB (76.77%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.py
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -1,17 +1,9 @@
 class Solution:
-    def maximum69Number (self, num: int) -> int:
-        ans=[]
-        k=num
-        while k>0:
-            ans.append(k%10)
-            k=k//10
-        ans1=0
-        print(ans)
-        f=True
-        for i in ans[::-1]:
-            if i==6 and f:
-                ans1=ans1*10+9
-                f=False
-            else:
-                ans1=ans1*10+i
-        return ans1
+    def maximum69Number(self, num: int) -> int:
+        digits = [int(d) for d in str(num)]  
+        for i in range(len(digits)):
+            if digits[i] == 6: 
+                digits[i] = 9
+                break
+        
+        return int(''.join(map(str, digits)))
