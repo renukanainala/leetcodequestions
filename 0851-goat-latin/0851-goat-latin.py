1b0fd54ce29b9270e8e248d116806b2a204e157b
renukanainala <142616816+renukanainala@users.noreply.github.com>
2025-08-30 21:58:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (56.17%) - LeetHub

--- 0851-goat-latin/0851-goat-latin.py
+++ 0851-goat-latin/0851-goat-latin.py
@@ -2,13 +2,13 @@ class Solution:
     def toGoatLatin(self, s: str) -> str:
         vo='aeiouAEIOU'
         s=s.split()
-        ans=[]
+        nw=""
         for i,w in enumerate(s):
             if w[0] in vo:
-                nw=w+'ma'
+                nw+=w+'ma'
             else:
-                nw=w[1:]+w[0]+'ma'
+                nw+=w[1:]+w[0]+'ma'
             nw+='a'*(i+1)
-            ans.append(nw)
-            
-        return " ".join(ans)
+            nw+=' '
+
+        return nw[:len(nw)-1]
